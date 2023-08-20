14d3060475d8f261afad032f99a4289f06c10f3f
Rohan Hasabe <rohanhasabe8@gmail.com>
2023-08-20 13:44:52 +0000

    parker: Part PE13; Part A13 update
    Signed-off-by: Rohan Hasabe <rohanhasabe8@gmail.com>

--- aosp_parker.mk
+++ aosp_parker.mk
@@ -17,15 +17,18 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from parker device
 $(call inherit-product, device/motorola/parker/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelExperience stuff.
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_parker
+PRODUCT_NAME := aosp_parker
 PRODUCT_DEVICE := parker
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := Motorola One Zoom
